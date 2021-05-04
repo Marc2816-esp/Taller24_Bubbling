c01de28f8f352e786334bc7d6c21e081fd8c3155
Marc2816-esp <marc.28.dani@gmail.com>
2021-05-04 17:23:29 -0600

    bubbuling JS

---
+++ script.js
@@ -0,0 +1,32 @@
+const btn = document.querySelector('button');
+const videoBox = document.querySelector('div#box');
+const video = document.querySelector('video');
+
+btn.onclick = function() {
+  videoBox.setAttribute('class','showing');
+}
+
+videoBox.addEventListener( 'click' , function(event) {
+  switch(event.target.id){
+    case 'play':
+      video.play();
+      break;
+    case 'pause':
+      video.pause();
+      break;
+    case 'stop':
+      video.pause();
+      video.currentTime=0;
+      break;
+    default:
+      videoBox.setAttribute('class','hidden');
+      break;
+  }
+  console.log('videoBox.onclick')
+},false);
+
+video.addEventListener('click' ,function(event) {
+  event.stopPropagation();
+  video.play();
+  console.log('videoBox.onclick');
+},false);
